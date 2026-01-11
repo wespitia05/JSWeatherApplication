0547087994baf4129fc04cacf76960da9e943ee7
william <espiw@farmingdale.edu>
2026-01-11 13:05:35 -0500

    weather app declaring our constants and functions we will be using

--- weather.js
+++ weather.js
@@ -1,1 +1,32 @@
 // ---------- WEATHER APP FUNCTIONALITY ---------- //
+
+// use querySelector bc its a class, not an id
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "0b27dee62b5909ebbbf9788a2e85e2b2";
+
+// event listener for when we press the submit button
+weatherForm.addEventListener("submit", event => {
+
+});
+
+// this function will get the city inputted and return data
+async function getWeatherData(city) {
+
+}
+
+// this function will gather specific data and display it
+function displayWeatherInfo(data) {
+
+}
+
+// this function will get the specific emoji for the weather id returned
+function getWeatherEmoji(weatherId) {
+
+}
+
+// this function will display any potential error messages
+function displayError(message) {
+
+}
